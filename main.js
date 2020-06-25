f856712d5d9d68e1fe83ed6bb7f4bfa853ffccf3
Linh My Dang <dangmylinhART@gmail.com>
2020-06-25 19:22:59 -0400

    exercise is done

--- main.js
+++ main.js
@@ -2,8 +2,69 @@
  * YOUR CODE HERE *
  ******************/
 
+ //// 1 Done ///////////////////////
+ function getFirstItemFrom(arr){
+ return arr[0]
+ }
+ let test1 = ['what','linh2','linh3']
+ let test2 = ['linh1','linh2','linh3']
+ 
+console.log(getFirstItemFrom(test2))
+console.log(getFirstItemFrom(test1))
 
 
+
+//// 2 Done  ///////////////////////
+function getLastItemFrom(arr){
+ return arr[arr.length-1]
+}
+let test3 = [1,2,3,4,'hoho']
+console.log(getLastItemFrom(test3))
+
+
+
+
+///3  Done ///////////////////////
+function getIndex3(arr){
+  if (!arr[3]){
+    return arr[arr.length-1]  
+  }else { return arr[3]}
+}
+let test4 = ['hihi', 'don', 'no']
+console.log(getIndex3(test4))
+
+
+
+/// 4 DONE ///////////////////////
+function isLongList(arr){
+if ( arr.length >= 10 ) { 
+  return  true 
+}  
+else { return false}
+}
+let test5 = ['hihi', 'don', 'no', 5,6,7,7,89,9,2,4,4,5]
+console.log (isLongList(test5))
+
+
+
+//5 DONEl
+ ///////////////////////
+function firstItemIsNumber(arr){
+  return arr[0] === arr[0]*1 ? true : false;
+}
+let test6 = [4,5,'bla',6,'hhe']
+let result1 = firstItemIsNumber(test6)
+result1;
+
+
+
+//6 DONE ///////////////////////
+function secondCharOfThirdString(arr){
+  let third = arr[3]
+  return arr[2][1]
+}
+test7 =['linh','huong','bla','blade','hhe']
+console.log(secondCharOfThirdString(test7))
 
 
 
